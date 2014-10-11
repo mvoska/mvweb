2b355101a8a49dfd38947ef46ec936a1cb8bf82b
Matt Voska <mattvoska@gmail.com>
2014-10-11 01:00:19 -0700

    Removes unnecessary files.

--- push_to_server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo -e '
-          Pushing Files to Server'
-scp index.html norad:/home/apache/mv/stable
-scp assets/css/style.min.css norad:/home/apache/mv/stable/assets/css
-scp assets/js/all.min.js norad:/home/apache/mv/stable/assets/js/
-scp -r assets/font/ assets/ico/ assets/img/ assets/resume/ norad:/home/apache/mv/stable/assets/
